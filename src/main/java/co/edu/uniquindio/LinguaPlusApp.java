5814736473e4d81ddeac95864f2f655e70f7801f
Javier Portela <javierj.portelaa@uqvirtual.edu.co>
2026-09-25 21:24:48 -0500

    Arreglo del conflicto en la vista de matricula

--- src/main/java/co/edu/uniquindio/LinguaPlusApp.java
+++ src/main/java/co/edu/uniquindio/LinguaPlusApp.java
@@ -10,7 +10,7 @@ public class LinguaPlusApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LinguaPlusApp.class.getResource("/MatriculaView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 460, 680);
         stage.setTitle("Gestión LinguaPlus");
         stage.setScene(scene);
         stage.show();
